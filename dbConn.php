7cd844d8283e9974cd01f662b3cb5c233100231d
Shehara Fernando <153703018+sheharaFer2001@users.noreply.github.com>
2025-06-24 19:37:51 +0530

    initial commit

---
+++ dbConn.php
@@ -0,0 +1,16 @@
+<?php
+
+   $hostname = "localhost";
+   $username = "root";
+   $password = "";  
+   $database = "crud_userdata";
+
+   $conn = mysqli_connect($hostname, $username, $password, $database);
+   if (!$conn) {
+       die("Connection failed: " . mysqli_connect_error());
+   }else {
+       // echo "Connected successfully";
+        echo "success";
+    }
+
+?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+<body>
+
+        <div class="container">
+            <table class="table table-hover table-bordererd table-striped">
+                <tr>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Age</th>
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>kamal</td>
+                    <td>Bandara</td>
+                    <td>26</td>
+
+                </tr>
+
+                <tr>
+                    <td>3</td>
+                    <td>kamal</td>
+                    <td>Bandara</td>
+                    <td>26</td>
+
+                </tr>
+            </table>
+        </div>
+    
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+</html>
